528652ffa84700e4a7b565982f3f539a645b332c
Ivan M <ivan.sm920@iguga.net>
2021-09-07 18:29:08 +0100

    Add some improvements

--- src/IgugaChecksumUtility.psm1
+++ src/IgugaChecksumUtility.psm1
@@ -9,7 +9,7 @@ $Script:LocalizedData = DATA {
     DiscoveringFilesProgressMessage=Discovering files, from '{0}'
     DiscoveringFilesProgressStatus=Running...
     ErrorInvalidArgument={0} parameter value is not valid
-    ErrorLoadingScript=There was an error loading '{0}'.
+    ErrorLoadingScript=There was an error loading '{0}': {1}
     ErrorPathNotFound=Cannot find path '{0}' because it does not exist.
     ErrorUtilityParameterRequired=To use {0} mode, the {1} parameter must be set correctly
     ErrorUtilityPathNotFound={0} mode was selected, but path '{1}' does not exists!
@@ -38,18 +38,20 @@ Import-LocalizedData -BindingVariable "Script:LocalizedData" -FileName IgugaChec
 
 # -------------------------- Load Script Files ----------------------------
 #
-$Public  = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public\*.ps1') -Recurse
+$Public  = @( Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1" -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path "$PSScriptRoot\Private\*.ps1" -ErrorAction SilentlyContinue )
 
-#Excluding the classes, because they will be loaded by the manifest
-$ModuleScriptFiles = Get-ChildItem -Path $PSScriptRoot -Filter *.ps1 | Where-Object { $_.Name -notlike "*.ps1xml" };
-
-foreach ($ScriptFile in $ModuleScriptFiles) {
-    try {
-        Write-Verbose -Message "$($Script:LocalizedData.VerboseLoadingScript -f $ScriptFile.FullName)"
-        . $ScriptFile.FullName
+#Dot source the files
+Foreach($Import in @($Public + $Private))
+{
+    Try
+    {
+        Write-Verbose -Message "$($Script:LocalizedData.VerboseLoadingScript -f $Import.FullName)"
+        . $Import.FullName
     }
-    catch {
-       Write-Error "$($Script:LocalizedData.ErrorLoadingScript -f $ScriptFile.FullName)"
+    Catch
+    {
+        Write-Error -Message $($Script:LocalizedData.ErrorLoadingScript -f $Import.FullName, $_)
     }
 }
 
